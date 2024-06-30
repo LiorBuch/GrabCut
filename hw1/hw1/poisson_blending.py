26c34ea53cb352690426fe83a98905a964a9de46
Liron-Lev <lironlev2604@gmail.com>
2024-06-30 15:10:14 +0300

    Start poisson blending

--- hw1/hw1/poisson_blending.py
+++ hw1/hw1/poisson_blending.py
@@ -2,25 +2,66 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse
+from scipy.sparse import lil_matrix, csc_matrix
 from scipy.sparse.linalg import spsolve
 import argparse
+from scipy.ndimage import binary_dilation
 
 
 def poisson_blend(im_src, im_tgt, im_mask, center):
     # LAPLACIAN OPERATOR
 
-    r_channel = im_src[:, :, 0]
-    g_channel = im_src[:, :, 1]
-    b_channel = im_src[:, :, 2]
+    mask_height,mask_width = im_mask.shape
+    num_pixels_to_paste = im_mask.shape[0]*im_mask.shape[1]
+    src_center = int(im_src.shape[0] / 2) , int(im_src.shape[1] / 2)
 
-    # POISSON SOLVE
+    relative_x = center[0] - src_center[0]
+    relative_y = center[1] - src_center[1]
+
+    # We build A according to the formulas in rec3
+    A = [lil_matrix((num_pixels_to_paste, num_pixels_to_paste), dtype='float64') for i in range(im_tgt.shape[2])]
+    b = [np.zeros(num_pixels_to_paste) for i in range(im_tgt.shape[2])]
+    f = []
+    for color in range(im_tgt.shape[2]):
+        print("color")
+        for i in range(mask_height):
+            print(f"row number {i} out of {mask_height}")
+            for j in range(mask_width):
+                if im_mask[i, j] >= 100:
+                    if i == j:
+                        A[color][i, j] = 4
+                    for k, m in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+                        if 0 < i + k < mask_height and 0 < j + m < mask_width:
+                            if im_mask[i + k, j + m] >= 100:
+                                A[color][i + k, j + m] = -1
+                            else:  # If on the edge of the mask
+                                # TODO: Check that not out of bound in im_tgt
+                                b[color][(i + k) * mask_width + (j + m)] += im_tgt[relative_x + i + k, relative_y + j + m, color]
+
+        # POISSON SOLVE
+        f.append(spsolve(A[color], b[color]))
 
     # BLEND
+    # boundary = get_boundary(im_mask)
+    for color in range(im_tgt.shape[2]):
+        for i in range(im_src.shape[0]):
+            for j in range(im_src.shape[1]):
+                if im_mask[i, j] >= 100:
+                    print(f"Pasting {i}, {j} -> {f[color][i * mask_width + j]}")
+                    im_tgt[relative_x+i,relative_y+j,color] = f[color][i * mask_width + j]
 
     # SAVE
 
     im_blend = im_tgt
     return im_blend
+
+def get_boundary(mask):
+    structure = np.ones((3, 3))
+    dilated_mask = binary_dilation(mask, structure=structure)
+    boundary = dilated_mask - mask
+    boundary_pixels = np.argwhere(boundary == 1)
+    return boundary_pixels
+
 
 
 def parse():
