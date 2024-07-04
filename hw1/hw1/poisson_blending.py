72a0e2df444068fae6a286c397b70cccf4ad52a8
LiorBuch <lior.buchmeier@gmail.com>
2024-07-04 19:02:25 +0300

    feat: new blend algorithm

--- hw1/hw1/poisson_blending.py
+++ hw1/hw1/poisson_blending.py
@@ -3,6 +3,7 @@ import math
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import numpy.linalg
 import scipy.sparse
 from scipy.sparse import lil_matrix, block_diag
 from scipy.sparse.linalg import spsolve
@@ -30,6 +31,7 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
     f = []
 
     bound = get_boundary(im_mask)
+
     for pixel in bound:
         for color in range(im_tgt.shape[2]):
             im_src[pixel[0],pixel[1],color] = im_tgt[pixel[0]+relative_h,pixel[1]+relative_w,color]
@@ -41,19 +43,20 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
             for j in range(mask_width):
                 idx = i * mask_width + j
                 # up down left right
-                # src_buffer = [0, 0, 0, 0]
-                # tgt_buffer = [0, 0, 0, 0]
+                src_buffer = [0, 0, 0, 0]
+                tgt_buffer = [0, 0, 0, 0]
                 if im_mask[i, j] >= 100:
                     A[color][idx, idx] = -4
                     # up
                     if 0 <= i - 1 < mask_height and 0 <= j <= mask_width:
                         if im_mask[i - 1, j] > 0:
-
                             A[color][idx, idx - mask_width] = 1
-                            b[color][idx] += (im_src[(i - 1), j, color] - im_src[i, j, color])
+                            src_buffer[0] =(im_src[(i - 1), j, color] - im_src[i, j, color])
+                            tgt_buffer[0] = (im_tgt[relative_h + (i - 1), relative_w + j, color] - im_tgt[
+                                relative_h + i, relative_w + j, color])
                         else:
                             A[color][idx, idx - mask_width] = 0
-                            b[color][idx] += (im_src[(i - 1), j, color] - im_src[i, j, color])
+                            #b[color][idx] += (im_src[(i - 1), j, color] - im_src[i, j, color])
                             b[color][idx] -= (im_tgt[relative_h + (i - 1), relative_w + j, color] - im_tgt[
                                 relative_h + i, relative_w + j, color])
 
@@ -61,11 +64,13 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
                     if 0 <= i + 1 < mask_height and 0 <= j <= mask_width:
                         if im_mask[i + 1, j] > 0:
                             A[color][idx, idx + mask_width] = 1
-                            b[color][idx] += (im_src[(i + 1), j, color] - im_src[i, j, color])
+                            src_buffer[1] = (im_src[(i + 1), j, color] - im_src[i, j, color])
+                            tgt_buffer[1] = (im_tgt[relative_h + (i + 1), relative_w + j, color] - im_tgt[
+                                relative_h + i, relative_w + j, color])
 
                         else:
                             A[color][idx, idx + mask_width] = 0
-                            b[color][idx] += (im_src[(i + 1), j, color] - im_src[i, j, color])
+                            #b[color][idx] += (im_src[(i + 1), j, color] - im_src[i, j, color])
                             b[color][idx] -= (im_tgt[relative_h + (i + 1), relative_w + j, color] - im_tgt[
                                 relative_h + i, relative_w + j, color])
 
@@ -73,11 +78,13 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
                     if 0 <= i < mask_height and 0 <= j - 1 <= mask_width:
                         if im_mask[i, j - 1] > 0:
                             A[color][idx, idx - 1] = 1
-                            b[color][idx] += (im_src[i, j - 1, color] - im_src[i, j, color])
+                            src_buffer[2]= (im_src[i, j - 1, color] - im_src[i, j, color])
+                            tgt_buffer[2]= (im_tgt[relative_h + i, relative_w + j - 1, color] - im_tgt[
+                                relative_h + i, relative_w + j, color])
 
                         else:
                             A[color][idx, idx - 1] = 0
-                            b[color][idx] += (im_src[i, j - 1, color] - im_src[i, j, color])
+                            #b[color][idx] += (im_src[i, j - 1, color] - im_src[i, j, color])
                             b[color][idx] -= (im_tgt[relative_h + i, relative_w + j - 1, color] - im_tgt[
                                 relative_h + i, relative_w + j, color])
 
@@ -85,13 +92,16 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
                     if 0 <= i < mask_height and 0 <= j - 1 <= mask_width:
                         if im_mask[i, j + 1] > 0:
                             A[color][idx, idx + 1] = 1
-                            b[color][idx] += (im_src[i, j + 1, color] - im_src[i, j, color])
-
+                            src_buffer[3]= (im_src[i, j + 1, color] - im_src[i, j, color])
+                            tgt_buffer[3]= (im_tgt[relative_h + i, relative_w + j + 1, color] - im_tgt[
+                                relative_h + i, relative_w + j, color])
                         else:
                             A[color][idx, idx + 1] = 0
-                            b[color][idx] += (im_src[i, j + 1, color] - im_src[i, j, color])
+                            #b[color][idx] += (im_src[i, j + 1, color] - im_src[i, j, color])
                             b[color][idx] -= (im_tgt[relative_h + i, relative_w + j + 1, color] - im_tgt[
                                 relative_h + i, relative_w + j, color])
+
+                    b[color][idx] = sum(src_buffer) - sum(tgt_buffer)
 
                 else:
                     A[color][idx, idx] = -1
@@ -105,19 +115,19 @@ def poisson_blend(im_src, im_tgt, im_mask, center):
         for i in range(mask_height):
             for j in range(mask_width):
                 if im_mask[i, j] >= 100:
-                    value = np.clip(abs(f[color][i * mask_width + j] + im_tgt[relative_h + i, relative_w + j, color]),0,255) # play with this?
+                    value = np.clip(abs((f[color][i * mask_width + j]*0.95 +im_tgt[relative_h+i,relative_w+j,color])),0,255) # play with this?
                     print(f"color:{color} value -> {value}")
                     im_tgt[relative_h + i, relative_w + j, color] = value
 
     # SAVE
-
+    print(len(bound))
     im_tgt = im_tgt.astype(np.uint8)
     im_blend = im_tgt
     return im_blend
 
 
 def get_boundary(mask):
-    structure = np.ones((3, 3))
+    structure = np.ones((3,3))
     dilated_mask = binary_dilation(mask, structure=structure)
     boundary = dilated_mask - mask
     boundary_pixels = np.argwhere(boundary == 1)
@@ -128,7 +138,7 @@ def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('--src_path', type=str, default='./data/imgs/banana2.jpg', help='image file path')
     parser.add_argument('--mask_path', type=str, default='./data/seg_GT/banana2.bmp', help='mask file path')
-    parser.add_argument('--tgt_path', type=str, default='./data/bg/table.jpg', help='mask file path')
+    parser.add_argument('--tgt_path', type=str, default='./data/bg/wall.jpg', help='mask file path')
     return parser.parse_args()
 
 
@@ -148,6 +158,7 @@ if __name__ == "__main__":
 
     im_clone = poisson_blend(im_src, im_tgt, im_mask, center)
     #im_clone = cv2.seamlessClone(im_src, im_tgt, im_mask, center, cv2.NORMAL_CLONE)
+
     cv2.imshow('Cloned image', im_clone)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
